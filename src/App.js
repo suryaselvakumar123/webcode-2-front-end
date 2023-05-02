790387867a97a370eb57e824eb4366ce82304a5f
suryaselvakumar123 <suryaselvakumar1007@gmail.com>
2023-05-02 12:53:43 +0530

    commit all

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
-import SignUp from './components/SignUp';
-import SignIn from './components/signin';
+
 import Info from './components/Info.js';
 import EditMovie from './components/Edit.js';
 import Add from './components/Add';
@@ -12,16 +11,17 @@ import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
+      
       <Routes>
-        <Route path="/contactus" element={<ContactUs />} />
-        <Route path="/addmovie" element={<Add />} />
-        <Route path="/edit/:movieid" element={<EditMovie />} />
-        <Route path="/info/:movieid" element={<Info />} />
-        <Route path="/adminboard" element={<AdminBoard />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/movieboard" element={<MovieBoard />} />
-        <Route path="/" element={<SignIn />} />
-      </Routes>
+  <Route path="/contactus" element={<ContactUs />} />
+  <Route path="/addmovie" element={<Add />} />
+  <Route path="/edit/:movieid" element={<EditMovie />} />
+  <Route path="/info/:movieid" element={<Info />} />
+  <Route path="/adminboard" element={<AdminBoard />} />
+  <Route path="/movieboard" element={<MovieBoard />} />
+  
+</Routes>
+
     </div>
   );
 }
